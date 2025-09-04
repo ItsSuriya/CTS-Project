4e455c2be11e88b4712d6ccd5849e83e4667e1a0
KishoreRajesh <kishorerajesh007@gmail.com>
2025-09-05 00:44:01 +0530

    fix:backend integration 2.0

--- frontend/src/components/AnalysisHero.jsx
+++
@@ -1,7 +0,0 @@
-import { BackgroundPaths } from "./ui/analysisHero";
-
-export function DemoBackgroundPaths() {
-    return (
-        <BackgroundPaths title="Upload your dataset to visualize and analyze patient information." />
-    );
-}
